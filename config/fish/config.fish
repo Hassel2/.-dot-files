e0d4be8c2af12d3ac639adf65f0b9aa9dc757d34
Hassel2 <hasselprassel@mail.ru>
2023-02-11 01:03:31 +0300

    Install bass and add support for nvm

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,3 +5,5 @@ end
 # tabtab source for electron-forge package
 # uninstall by removing these lines or running `tabtab uninstall electron-forge`
 [ -f /home/anton/proj/simple-desktop-app-electronjs/node_modules/tabtab/.completions/electron-forge.fish ]; and . /home/anton/proj/simple-desktop-app-electronjs/node_modules/tabtab/.completions/electron-forge.fish
+
+set -x NVM_DIR ~/.nvm
