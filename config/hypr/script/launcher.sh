6def73a5701620362b18dd3f4472d1c3758959f0
Hassel2 <hasselprassel@mail.ru>
2023-04-19 17:38:24 +0300

    style(rofi): change rofi appearance

--- config/hypr/script/launcher.sh
+++ config/hypr/script/launcher.sh
@@ -2,9 +2,6 @@ dir="$HOME/.config/hypr/rofi/"
 theme='applauncher'
 
 ## Run
-grim - > ~/.temp/rofi.png && ffmpeg -i ~/.temp/rofi.png -vf "boxblur=10" -c:a copy ~/.temp/rofi_copy.png -y 
-killall -SIGUSR1 waybar &
 rofi \
     -show drun \
     -theme ${dir}/${theme}.rasi
-killall -SIGUSR1 waybar
