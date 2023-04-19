87430f978c3b2f88eacaee2a2fef6c6446134a65
Hassel2 <hasselprassel@mail.ru>
2023-04-19 17:41:57 +0300

    feat(hyprland): change volume script to work with pipewire

--- config/hypr/script/volume.sh
+++ config/hypr/script/volume.sh
@@ -1,1 +1,12 @@
-notify-send -h int:value:$(ponymix -c 0 $1 1) -h string:synchronous:my-progress "Volume info" -i ~/.temp/mic.png
+vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+if [[ "$vol" != *"MUTED"* ]]; then
+    lvl=${vol//Volume: /}
+    if [[ "$lvl" != "1.00" ]]; then
+        wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%$1
+    elif [[ "$lvl" == "1.00" ]] && [[ "$1" == "-" ]]; then
+        wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%$1
+    fi;
+    vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+    lvl=${vol//Volume: /}
+    notify-send -h int:value:$(echo "100 * $lvl" | bc) -h string:synchronous:my-progress "Volume info" -i ~/.temp/mic.png
+fi;
