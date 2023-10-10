5feb31ce6e1c54107b3d8aed3179674affd33b9f
Hassel2 <hasselprassel@mail.ru>
2023-10-10 10:17:24 +0300

    style(waybar): apply everforest theme

--- config/waybar/script/workspaces.sh
+++ config/waybar/script/workspaces.sh
@@ -1,21 +1,21 @@
 #! /usr/bin/env cached-nix-shell
 #! nix-shell -i bash -p bash
-#
+
 active=($( hyprctl workspaces -j | jq .[].id ));
 current=($( hyprctl monitors -j | jq .[].activeWorkspace.id ))
 
 workspaces='{"text":"'
 
 for (( i=1; i<=10; i++ )) do
-    workspaces=$workspaces'<span color=\"#1e1e2e\">.</span>'
+    workspaces=$workspaces'<span color=\"#272E33\">.</span>'
     if [[ " ${current[*]} " =~ " ${i} " ]]; then
-        workspaces=$workspaces'<span color=\"#cdd6f4\"><big></big></span>'
+        workspaces=$workspaces'<span color=\"#A7C080\"><big></big></span>'
     elif [[ " ${active[*]} " =~ " ${i} " ]]; then
-        workspaces=$workspaces'<span color=\"#cdd6f4\"><big></big></span>'
+        workspaces=$workspaces'<span color=\"#A7C080\"><big></big></span>'
     else
-        workspaces=$workspaces'<span color=\"#313244\"><big></big></span>'
+        workspaces=$workspaces'<span color=\"#56635f\"><big></big></span>'
     fi;
-    workspaces=$workspaces'<span color=\"#1e1e2e\">.</span>'
+    workspaces=$workspaces'<span color=\"#272E33\">.</span>'
 done;
 
 workspaces=$workspaces'"}'
