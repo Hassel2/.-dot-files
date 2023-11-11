21861fefc4fb51424a4696b0a6b7dd7a3b6e78a3
Hassel2 <hasselprassel@mail.ru>
2023-11-11 21:59:09 +0300

    feat(waybar): change colors

--- config/waybar/script/workspaces.sh
+++ config/waybar/script/workspaces.sh
@@ -7,15 +7,15 @@ current=($( hyprctl monitors -j | jq .[].activeWorkspace.id ))
 workspaces='{"text":"'
 
 for (( i=1; i<=10; i++ )) do
-    workspaces=$workspaces'<span color=\"#272E33\">.</span>'
+    workspaces=$workspaces'.'
     if [[ " ${current[*]} " =~ " ${i} " ]]; then
-        workspaces=$workspaces'<span color=\"#A7C080\"><big></big></span>'
+        workspaces=$workspaces'<span color=\"#A7C080\"></span>'
     elif [[ " ${active[*]} " =~ " ${i} " ]]; then
-        workspaces=$workspaces'<span color=\"#A7C080\"><big></big></span>'
+        workspaces=$workspaces'<span color=\"#A7C080\"></span>'
     else
-        workspaces=$workspaces'<span color=\"#56635f\"><big></big></span>'
+        workspaces=$workspaces'<span color=\"#56635f\"></span>'
     fi;
-    workspaces=$workspaces'<span color=\"#272E33\">.</span>'
+    workspaces=$workspaces'.'
 done;
 
 workspaces=$workspaces'"}'
