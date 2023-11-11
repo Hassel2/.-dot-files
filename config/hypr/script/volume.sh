8314fd016cccce8afef76862511e024fc97b448e
Hassel2 <hasselprassel@mail.ru>
2023-11-11 22:02:03 +0300

    feat(hypr/volume-control-script): set limit max limit to 1.5 by wpctl flag

--- config/hypr/script/volume.sh
+++ config/hypr/script/volume.sh
@@ -3,13 +3,8 @@
 
 vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
 if [[ "$vol" != *"MUTED"* ]]; then
-    lvl=${vol//Volume: /}
-    if [[ "$lvl" != "1.00" ]]; then
-        wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%$1
-    elif [[ "$lvl" == "1.00" ]] && [[ "$1" == "-" ]]; then
-        wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%$1
-    fi;
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%$1 --limit 1.5
     vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
     lvl=${vol//Volume: /}
-    notify-send -h int:value:$(echo "100 * $lvl" | bc) -h string:synchronous:my-progress "Volume info" -i ~/.temp/mic.png
+    notify-send -h int:value:$(echo "66.66666666 * $lvl" | bc) -h string:synchronous:my-progress "Volume info" -i ~/.temp/mic.png
 fi;
